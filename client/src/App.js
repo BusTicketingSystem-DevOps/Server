2bbbcc58a689b9aeaa34a6b97776b1a553838630
Kiran Kumar B <kiran0317@gmail.com>
2023-04-23 16:51:03 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import {Button} from 'antd';
+
+function App() {
+  return (
+    <div className='App m-5'>
+      <h1>Bus Ticketing System</h1>
+      <Button type="primary">Primary Button</Button>
+
+    </div>
+  );
+}
+
+export default App;
