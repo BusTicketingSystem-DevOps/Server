6ecb328a511dbcd1753e4fed95982622fb1e7723
Kiran Kumar B <kiran0317@gmail.com>
2023-05-09 19:34:31 +0530

    Added Stripe Gateway

--- routes/bookingsRoute.js
+++ routes/bookingsRoute.js
@@ -1,7 +1,9 @@
+const stripe = require("stripe")("sk_test_51N5JrmSJRynLcBpMJwX06OmR6ld25XhPyntpJMShkncosF8eLkTnwqfPRRb5ZcxcIzbmmeuGfAunDzEe4VFhrMFa00kGXi4gmQ");
 const router = require("express").Router();
 const authMiddleware = require("../middlewares/authMiddleware");
 const Booking = require("../models/bookingsModel");
 const Bus = require("../models/busModel");
+const { v4: uuidv4 } = require("uuid");
 
 // book a seat
 
@@ -9,7 +11,6 @@ router.post("/book-seat", authMiddleware, async (req, res) => {
     try {
       const newBooking = new Booking({
         ...req.body,
-        transactionId: "1234",
         user: req.body.userId,
       });
       await newBooking.save();
@@ -29,6 +30,55 @@ router.post("/book-seat", authMiddleware, async (req, res) => {
       });
     }
   });
+
+  // make payment
+
+router.post("/make-payment", authMiddleware, async (req, res) => {
+  try {
+    const { token, amount } = req.body;
+    const customer = await stripe.customers.create({
+      email: token.email,
+      source: token.id,
+    });
+    const payment = await stripe.paymentIntents.create(
+      {
+        amount: amount,
+        currency: "inr",
+        customer: customer.id,
+        receipt_email:token.email,
+        automatic_payment_methods: {
+          enabled: true,
+        },
+      },
+      {
+        idempotencyKey: uuidv4(), //To avoid charging multiple times for the same customer
+      }
+    );
+
+    if (payment) {
+      res.status(200).send({
+        message: "Payment successful",
+        data: {
+          transactionId: payment.id
+        },
+        success: true,
+      });
+    } else {
+      res.status(500).send({
+        message: "Payment failed",
+        data: error,
+        success: false,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      message: "Payment failed",
+      data: error,
+      success: false,
+    });
+  }
+});
 
   module.exports = router;
   
