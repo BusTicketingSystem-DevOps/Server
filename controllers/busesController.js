8c1ec52ff0edc188b4416df59ddb0ccd9f249ebb
KiranKumar-B <kiran0317@gmail.com>
2023-05-14 17:24:59 +0530

    Minor fix

--- controllers/busesController.js
+++ controllers/busesController.js
@@ -8,7 +8,7 @@ exports.addBus = async (req, res) => {
     const existingBus = await Bus.findOne({ number: req.body.number });
     if (existingBus) {
       busesLogger.warn(`${req.body.number} add - already_exists`);
-      return res.status(200).send({
+      return res.status(400).send({
         success: false,
         message: "Bus already exists",
       });
